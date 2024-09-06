5ec81ea745cd732bfbbe663fd485e878276ad618
KatKatKateryna <kateryna@speckle.systems>
2024-09-06 18:12:09 +0100

    fixes

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ source .venv/bin/activate
 
 export PYGEOAPI_CONFIG="example-config.yml"
 export PYGEOAPI_OPENAPI="example-openapi.yml"
-export MAPTILER_KEY_LOCAL="qam9vwl7bVk5tW1oZu46"
+export MAPTILER_KEY_SPECKLE="qam9vwl7bVk5tW1oZu46"
 export PORT=8000
 
 gunicorn pygeoapi.flask_app:APP --timeout 100000 --access-logfile access_log --error-logfile error_log --capture-output
